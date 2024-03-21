fb80244742de90278a36aaea3858fcd8f9a23ab9
mingCoder1234 <mingcoder1234@gmail.com>
2024-03-21 19:24:02 +0700

    first commit

---
+++ main.py
@@ -0,0 +1,25 @@
+import matplotlib.pyplot as plt
+
+
+def read_input():
+  file_url = open("data/file.txt", "r").read()
+  angle = []
+  time = []
+  input_file = open(file_url, "r")
+  for line in input_file:
+    a, t = map(float, line.split())
+    angle.append(a)
+    time.append(t)
+
+  return [angle, time]
+
+def main():
+  angle, time = read_input()
+  plt.plot(time, angle)
+  plt.ylabel('time')
+  plt.xlabel('angle')
+  plt.show()
+  pass
+
+if __name__ == '__main__': 
+  main()
---
+++ readme.md
@@ -0,0 +1,2 @@
+This is the PID visualizer from team 25812M
+To use, please replace the content in data/file.txt by the url to the data file, or adjust the content in data.txt
